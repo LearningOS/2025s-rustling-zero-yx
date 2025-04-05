bcda2b48b2b954abcd7d730309062257551d3360
XuLanshuo <2476874454@qq.com>
2025-04-05 16:47:06 +0000

    solve macros and modules quiz

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -9,7 +9,7 @@
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    };
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
     }
@@ -19,3 +19,4 @@ fn main() {
     my_macro!();
     my_macro!(7777);
 }
+
